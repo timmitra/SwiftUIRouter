e611c50edaf04cafd96bd196b2f6c0648d3fadd9
Tim Mitra <tim@it-guy.com>
2025-01-15 11:52:22 -0500

    Initial Commit

---
+++ SwiftUIRouter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIRouter
+//
+//  Created by Tim Mitra on 2025-01-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIRouter/SwiftUIRouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRouterApp.swift
+//  SwiftUIRouter
+//
+//  Created by Tim Mitra on 2025-01-15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
